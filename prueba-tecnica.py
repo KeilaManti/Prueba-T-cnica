438eb095edcd1c5cf93e1dedfe286273bd2e15dd
KeilaManti <keila_manti@hotmail.com.ar>
2024-03-12 13:08:10 -0300

    Prueba técnica

---
+++ prueba-tecnica.py
@@ -0,0 +1,26 @@
+import os
+
+def buscarPalabra(archivo, palabra):
+    cantidad = 0
+    archivoLeido = open(archivo, encoding="utf8")
+    lineasArchivo = []
+    if os.stat(archivo).st_size == 0:
+        print("El archivo estÃƒÂ¡ vacio")
+    for linea in archivoLeido:
+        linea = linea[0:-1]
+        c = linea.count(palabra)
+        cantidad=cantidad+c
+        if c > 0:
+            lineasArchivo.append(linea)
+    archivoLeido.close()
+    return [cantidad, lineasArchivo]
+
+palabra = "dragones"
+archivo = "La comunidad del anillo.txt"
+resultado = buscarPalabra(archivo, palabra)
+print("La cantidad de veces que aparece la palabra", palabra, "es :", resultado[0], "\n")
+print("Las lineas donde apareció la palabra son:")
+
+for linea in resultado[1]:
+    print(linea)
+
